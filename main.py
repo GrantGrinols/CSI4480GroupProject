02112951089678c3b86c78ca9776aa12e513cc82
GrantGrinols <grantgrinols@oakland.edu>
2023-10-26 21:02:38 -0400

    CompareVoices and GraphVoices Added

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import elevenlabs
 import wave
 import json
+import librosa
+import pandas as pd
+import matplotlib.pylab as plt
+import librosa.display
+from pydub import AudioSegment
+from pydub.utils import mediainfo
+from resemblyzer import VoiceEncoder,preprocess_wav
+import numpy as np
 
 jsonFile = open("APIKey.json")
 key = json.load(jsonFile)
@@ -31,23 +39,54 @@ def GenerateVoice(text, name):
         with wave.open(filename, 'w') as wave_file:
             wave_file.setnchannels(2)
             wave_file.setsampwidth(2)
-            wave_file.setframerate(44100)
+            wave_file.setframerate(8500)
             wave_file.writeframes(voicesound)
-        return filename #name of the new ai voice file
+        return filename #path of the new ai voice file
        
     else:
         print("Voice not found. Please try again")
 
 def CompareVoice(generatedvoice, realvoice):
-    #print out the graph of each voice
+   ## DrawGraph(generatedvoice, realvoice)
+   
     #Find some way to compare generatevoice with realvoice
     #print out the similarity
-    print("Work in progress")
+    encoder = VoiceEncoder()
+    processedfile1 = preprocess_wav(generatedvoice)
+    processedfile2 = preprocess_wav(realvoice)
+    embed1 = encoder.embed_utterance(processedfile1)
+    embed2 = encoder.embed_utterance(processedfile2)
+    dot_product_size = np.dot(embed1, embed2)
+    norm_sound1 = np.linalg.norm(embed1)
+    norm_sound2 = np.linalg.norm(embed2)
+
+    similarity = dot_product_size / (norm_sound1 * norm_sound2)
+    similarity = similarity * 100
+    similarity= int(similarity)
+    print("Similarity: %"+ str(similarity))
 
     
 
+def DrawGraph(AIVoice, realVoice): 
+    #print out the graph of each voice
+    y1, sr1 = librosa.load(AIVoice)
+    y2, sr2 = librosa.load(realVoice)
+    
+    time1 = librosa.times_like(y1, sr=sr1)
+    time2 = librosa.times_like(y2, sr=sr2)
+    plt.subplot(2,1,1)
+    plt.title("AI Audio")
+    plt.ylabel('Amplitude')
+    pd.Series(y1).plot(figsize=(10,5))
+    plt.subplot(2,1,2)
+    plt.title("Real Audio")
+    plt.ylabel('Amplitude')
+    pd.Series(y2).plot(figsize=(10,5))
+    plt.show()
+CompareVoice("audio_data/0ghm5Cqpfwk.mp3","AIclips/4glfwiMXgwQ.mp3") 
 
 option =0
+#DrawGraph("AIClips/VoiceclipKyleVoice.wav","audio_data/Hello.wav")
 print("Hello, and welcome to the AI voice mimic!")
 print("Remember to set your API key!")
 SetKey(KEY)
@@ -66,6 +105,7 @@ while(option!=-1):
         GenerateVoice(text, voice)
     if(option==3):
         realvoicefile= input("Enter the file name of the voice you want to compare: ")
+        realvoicefile = "audio_data/"+realvoicefile+".wav"
         voice = input("Enter the relevent AI voice: ")
         text = input("Enter the text the voice is trying to mimic: ")
         generatedvoiceFile = GenerateVoice(text, voice)
