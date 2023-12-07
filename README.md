360a4546910ce64c5bf8354000d0bc70daeb402f
GrantGrinols <grantgrinols@oakland.edu>
2023-12-07 16:04:39 -0500

    Updated README

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # CSI4480GroupProject
 A project concerning mimicking real people's voices with the help of AI.
+## Overview
 
-Required Python libraries: elevenlabs, json, resemblyzer, matplotlib, numpy, os
-Make sure you've made a 'APIKey.json' folder with "APIKey": (your key as a string)
+This project hosts a local website (done via the Flask framework) on your machine. On this website, you can generate AI voice clips and compare these voice clips to other predefined voice samples. Additionally, you can also clone and delete custom voices, should you have paid access to Elevenlabs' exclusive features.
+
+This project also allows for automated testing with Selenium Webdriver by running Selenium.py while main.py is already running. This is particularly useful if you want to get similarity rates in batch or using this software against voice recognition software.
+
+
+
+## Requirements
+Required Python libraries: elevenlabs, json, resemblyzer, matplotlib, numpy, os, pandas, flask, random
+Optional Python libraries (for Selenium.py): selenium, time
+Make sure you've made a 'APIKey.json' file with "APIKey": (your key as a string)
 ffmpeg.exe is required to play the files. Download it here: https://ffmpeg.org/
+In order for this project to function, you need an Elevenlabs API key. You can get an API key by making an account at https://elevenlabs.io/
--- Selenium.py
+++ Selenium.py
@@ -9,6 +9,10 @@ import time
 
 #setting up the webdriver
 
+
+#In order to use this, make sure main.py is running first to boot up the local host.
+#Also run this on a seperate window/console to make sure the two consoles don't overlap each other
+
 service = Service()
 options = webdriver.EdgeOptions()
 driver = webdriver.Edge(service=service, options= options)
@@ -17,12 +21,13 @@ driver = webdriver.Edge(service=service, options= options)
 wait = WebDriverWait(driver, 20)
 
 #How many tests we will perform
-maxcount = 5
+maxcount = 10
 currentcount = 0
-nametoselect = "Kyle2"
-similarityarray = []
+nametoselect = "Grant" #name of the clone
+similarityarray = [] #array that has our results
 driver.get("http://127.0.0.1:5000/compare")
 wait.until(EC.presence_of_element_located((By.TAG_NAME,'body')))
+print("Beginning Testing...")
 while(currentcount<maxcount):
     
     wait.until(EC.presence_of_element_located((By.TAG_NAME,'body')))##WAIT until the page is loaded
@@ -31,7 +36,7 @@ while(currentcount<maxcount):
     input_field = driver.find_element(By.ID,'input_field') #enter the input to input box
     input_field.send_keys("Alexa... who am I?")
     path = driver.find_element(By.ID, 'path_to_real_voice')
-    path.send_keys('static/audiodata/KyleDo2.wav')
+    path.send_keys('static/audiodata/Grant2.wav')
     button = driver.find_element(By.ID,'comparebutton')
     #breakpoint()
     button.click()#click the button
@@ -44,7 +49,7 @@ while(currentcount<maxcount):
     similarity = similarity *.01
     similarity = round(similarity, 3)
     similarityarray.append(similarity)
-    time.sleep(5) #give some time to give the human tester to write the needed information down
+    time.sleep(7) #give some time to give the human tester to write the needed information down and for audio/alexa to play out
 driver.close()
 print("Testing concluded!")
 print("Results:")
